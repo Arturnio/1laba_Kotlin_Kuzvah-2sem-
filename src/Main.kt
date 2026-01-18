59220713f29f22556cb3c583764c3f6df5e77889
Artur <only.up.ak@gmail.com>
2026-01-18 18:34:43 +0300

    Add abstract modules, interfaces andfirst moduleimplementations

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,21 @@
 fun main() {
-    val manager = ResourceManager()
-    val minerals = OutpostResource(id = 1, name = "Minerals", amount = 300)
-    val gas = OutpostResource(id = 2, name = "Gas", amount = 100)
+//    val manager = ResourceManager()
+//    val minerals = OutpostResource(id = 1, name = "Minerals", amount = 300)
+//    val gas = OutpostResource(id = 2, name = "Gas", amount = 100)
+//
+//    manager.add(minerals)
+//    manager.add(gas)
+//    manager.printAll()
+//    val bonus = minerals.copy(amount = minerals.amount + 50)
+//    println("Копия минералов с бонусом $bonus")
 
-    manager.add(minerals)
-    manager.add(gas)
+    val manager = ResourceManager()
+    manager.add(OutpostResource(id = 1, name = "Minerals", amount = 120))
+    manager.add(OutpostResource(id = 2, name = "Gas", amount = 40))
+    val generator = EnergyGenerator()
+    val lab = ResearchLab()
+    generator.performAction(manager)
+    lab.performAction(manager)
+    println()
     manager.printAll()
-    val bonus = minerals.copy(amount = minerals.amount + 50)
-    println("Копия минералов с бонусом $bonus")
 }
